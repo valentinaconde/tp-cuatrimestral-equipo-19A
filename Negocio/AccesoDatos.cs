44f463c65460e4b98d860a17e896c1522132b267
valentinaconde <valencondde@gmail.com>
2024-10-15 18:11:37 -0300

    agregado de libreria de clases dominio negocio y agregado de clase acceso a datos

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public SqlCommand Comando
+        {
+            get { return comando; }
+        }
+
+        public SqlConnection Conexion
+        {
+            get { return conexion; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=COMERCIO_DB_19A; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        public void abrirConexion()
+        {
+            conexion.Open();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+
+    }
+}
