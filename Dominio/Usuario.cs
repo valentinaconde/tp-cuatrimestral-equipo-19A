bc53f88dc84ead8a0a756c546dc34d1a2f1544bd
valentinaconde <valencondde@gmail.com>
2024-10-21 15:22:15 -0300

    agregado de usuario al dominio

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Usuario
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public string rol_id { get; set; }
+
+
+    }
+}
