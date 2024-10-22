4ed67930736112c979f4bc99f0fe3ede854716b8
milidub <milagrosdubuis@hotmail.com>
2024-10-22 17:21:05 -0300

    agregado marcanegocio

---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class MarcaNegocio
+    {
+        public List<Marca> listar()
+        {
+            AccesoDatos datos = new AccesoDatos();
+            List<Marca> lista = new List<Marca>();
+
+            try
+            {
+                datos.setearConsulta("select id, nombre from marcas");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.id = (int)datos.Lector["id"];
+                    aux.nombre = datos.Lector["nombre"].ToString();
+
+                    lista.Add(aux);
+                }
+      
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void agregar(string nombre)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("insert into marcas (nombre) values (@nombre)");
+                datos.setearParametro("@nombre", nombre);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(int id, string nombre)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("update marcas set nombre = @nombre where id = @id");
+                datos.setearParametro("@id", id);
+                datos.setearParametro("@nombre", nombre);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                // datos.setearParametro("@id", id);
+                //  datos.setearConsulta("UPDATE ARTICULOS SET IdMarca = NULL WHERE IdMarca = @id");
+                // datos.ejecutarAccion();
+
+                datos.setearConsulta("delete from marcas where id = @id");
+                datos.setearParametro("@id", id);
+                datos.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
+ 
+
