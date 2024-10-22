db5212ff8973da0681e16fa3bd247691c04f90a3
unknown <les.yparraguirre@alumnos.frgp.utn.edu.ar>
2024-10-22 00:28:23 -0300

    Agregado de clase producto

---
+++ Dominio/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int stock { get; set; }
+        public float ganancia { get; set; }
+        public int idmarca { get; set; }
+        public int idcategoria { get; set; }
+
+
+    }
+}
