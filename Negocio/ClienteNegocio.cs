200b7dd96ff103c6c5aa07854603bbddfda2503a
milidub <milagrosdubuis@hotmail.com>
2024-10-22 19:23:33 -0300

    agregado clase clientenegocio y algunas funcionalidades (falto baja)

--- Negocio/ClienteNegocio.cs
+++ Negocio/ClienteNegocio.cs
@@ -5,28 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Dominio;
 
+
 namespace Negocio
 {
-    public class ProveedorNegocio
+    public class ClienteNegocio
     {
 
-        public List<Proveedor> listar()
+        public List<Cliente> listar()
         {
             AccesoDatos datos = new AccesoDatos();
-            List<Proveedor> lista = new List<Proveedor>();
-            
+            List<Cliente> lista = new List<Cliente>();
+
 
             try
             {
-                datos.setearConsulta("select id, nombre, cuit, direccion, telefono, email from proveedores");
+                datos.setearConsulta("select * from clientes");
                 datos.ejecutarLectura();
 
                 while (datos.Lector.Read())
                 {
-                    Proveedor aux = new Proveedor();
+                    Cliente aux = new Cliente();
                     aux.id = (int)datos.Lector["id"];
                     aux.nombre = datos.Lector["nombre"].ToString();
-                    aux.cuit = datos.Lector["cuit"].ToString();
+                    aux.apellido = datos.Lector["apellido"].ToString();
                     aux.direccion = datos.Lector["direccion"].ToString();
                     aux.telefono = datos.Lector["telefono"].ToString();
                     aux.email = datos.Lector["email"].ToString();
@@ -35,7 +36,7 @@ namespace Negocio
                     lista.Add(aux);
                 }
 
-                
+
                 return lista;
             }
             catch (Exception ex)
@@ -48,14 +49,14 @@ namespace Negocio
             }
         }
 
-        public void agregar(string nombre, string cuit, string direccion, string telefono, string email)
+        public void agregar(string nombre, string apellido, string direccion, string telefono, string email)
         {
             AccesoDatos datos = new AccesoDatos();
             try
             {
-                datos.setearConsulta("insert into proveedores (nombre, cuit, direccion, telefono, email) values (@nombre, @cuit, @direccion, @telefono, @correo)");
+                datos.setearConsulta("insert into clientes (nombre, apellido, direccion, telefono, email) values (@nombre, @apellido, @direccion, @telefono, @correo)");
                 datos.setearParametro("@nombre", nombre);
-                datos.setearParametro("@cuit", cuit);
+                datos.setearParametro("@apellido", apellido);
                 datos.setearParametro("@direccion", direccion);
                 datos.setearParametro("@telefono", telefono);
                 datos.setearParametro("@correo", email);
@@ -71,14 +72,14 @@ namespace Negocio
             }
         }
 
-        public void modificar(int id, string nombre, string cuit, string direccion, string telefono, string email)
+        public void modificar(int id, string nombre, string apellido, string direccion, string telefono, string email)
         {
             AccesoDatos datos = new AccesoDatos();
             try
             {
-                datos.setearConsulta("update proveedores set nombre = @nombre, apellido = @apellido, direccion = @direccion, telefono = @telefono, email = @correo where id = @id");
+                datos.setearConsulta("update clientes set nombre = @nombre, apellido = @apellido, direccion = @direccion, telefono = @telefono, email = @correo where id = @id");
                 datos.setearParametro("@nombre", nombre);
-                datos.setearParametro("@cuit",cuit);
+                datos.setearParametro("@apellido", apellido);
                 datos.setearParametro("@direccion", direccion);
                 datos.setearParametro("@telefono", telefono);
                 datos.setearParametro("@correo", email);
@@ -94,6 +95,7 @@ namespace Negocio
                 datos.cerrarConexion();
             }
         }
+
 
         public void eliminar(int id)
         {
@@ -101,8 +103,10 @@ namespace Negocio
             try
             {
                 datos.setearParametro("@id", id);
+                ///revisar: datos.setearConsulta("delete from proveedores where id = @id");
+                
 
-                datos.setearConsulta("delete from proveedores where id = @id");
+                
                 datos.ejecutarAccion();
             }
             catch (Exception ex)
@@ -115,6 +119,7 @@ namespace Negocio
             }
         }
 
+
+
     }
 }
-
