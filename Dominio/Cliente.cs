c56950583ebedbd16d69914e1f91356d367908a5
milidub <milagrosdubuis@hotmail.com>
2024-10-22 19:04:20 -0300

    agregado clase cliente

---
+++ Dominio/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+    }
+}
