154d07fe866c72205379a5e4f447ab6eea29a4b5
milidub <milagrosdubuis@hotmail.com>
2024-10-30 01:58:54 -0300

    clase venta

---
+++ Dominio/Venta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Venta
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public float total { get; set; }
+        public string nroFactura { get; set; }
+        public int clienteID { get; set; }
+        public int usuarioID { get; set; }
+        public List<DetalleVenta> Detalles {  get; set; }
+    }
+}
